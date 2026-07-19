d8ba2ecb226b79c62ab15b145401fd73c3996142
a-mhamdi <a_mhamdi@outlook.com>
2026-07-19 01:14:16 +0100

    configuration

---
+++ codes/esp32-cam/include/config.example.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+
+namespace config
+{
+    constexpr char WIFI_SSID[]     = "<SSID>";
+    constexpr char WIFI_PASSWORD[] = "<PASSWORD>";
+
+    const int SERVER_PORT = 80;
+    constexpr char ENDPOINT[] = "/capture";
+}
